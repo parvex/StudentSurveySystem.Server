f364ad02b9e196bd5cdefb5b2a52c5522abba114
Gabriel Rębacz <gabriel.rebacz@enigma.com.pl>
2020-03-25 02:34:52 +0100

    fixes

--- Core/Models/AuthenticateDto.cs
+++ Core/Models/AuthenticateDto.cs
@@ -1,13 +1,9 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace StudentSurveySystem.Core.Models
+﻿namespace StudentSurveySystem.Core.Models
 {
     public class AuthenticateDto
     {
-        [Required]
         public string Username { get; set; }
 
-        [Required]
         public string Password { get; set; }
     }
 }
