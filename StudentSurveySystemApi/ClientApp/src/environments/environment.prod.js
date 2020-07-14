6c36c77b17404c84b96f6150e34527e7cb74f448
Gabriel Rębacz <gabrielrebacz@gmail.com>
2020-07-14 17:39:28 +0200

    Revert "prod api path"
    This reverts commit 6cbb7ffe6c804de28ba16b0f807d373330c15da6.

--- StudentSurveySystemApi/ClientApp/src/environments/environment.prod.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.environment = void 0;
-exports.environment = {
-    production: true,
-    API_PATH: 'https://server20200331200409.azurewebsites.net/'
-};
-//# sourceMappingURL=environment.prod.js.map
--- StudentSurveySystemApi/ClientApp/src/environments/environment.prod.ts
+++ StudentSurveySystemApi/ClientApp/src/environments/environment.prod.ts
@@ -1,4 +1,3 @@
 export const environment = {
-  production: true,
-  API_PATH: 'https://server20200331200409.azurewebsites.net/'
+  production: true
 };
