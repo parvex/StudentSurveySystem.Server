6cbb7ffe6c804de28ba16b0f807d373330c15da6
Gabriel Rębacz <gabrielrebacz@gmail.com>
2020-07-14 16:55:18 +0200

    prod api path

---
+++ StudentSurveySystemApi/ClientApp/src/environments/environment.prod.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.environment = void 0;
+exports.environment = {
+    production: true,
+    API_PATH: 'https://server20200331200409.azurewebsites.net/'
+};
+//# sourceMappingURL=environment.prod.js.map
